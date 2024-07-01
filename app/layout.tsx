d0fcde3be1961873f9be7bab23ed7a477b6cbe18
UdreaLucas <udrealucas@gmail.com>
2024-07-01 12:01:42 -0300

    chore: Update portfolio title and description
    ```

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Lucas's Portfolio",
+  description: "Lucas Udrea Portfolio",
 };
 
 export default function RootLayout({
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -61,7 +61,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "udrealucas@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
